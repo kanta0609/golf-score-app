091632a08dc23a8131322583ff7ef19bfd422d8a
kanta0609 <kantakomuro0609@gmail.com>
2024-07-31 23:22:50 +0900

    MUIの導入（表のレイアウトの決定）

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import * as React from "react";
+import BasicTable from "./components/BasicTable"; // BasicTable コンポーネントのパスを適切に設定
 
 function App() {
-  return <>hello world</>;
+  return (
+    <>
+      <BasicTable />
+    </>
+  );
 }
 
 export default App;
